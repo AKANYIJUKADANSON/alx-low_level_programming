c32a335ee3efeba5ed0a36cba1d892f4ecc00efa
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2023-02-20 06:14:01 -0800

    Printing the alphabet in lowercase, followed by a new line without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - printing the alphabet in lowercase,
+ * without q and e
+ * followed by new line
+ * Return:(0) is a success
+ */
+int main(void)
+{
+	char ch;
+	char CH;
+
+	for (ch = 'a'; ch <= 'd'; ch++)
+	{
+		putchar(ch);
+	}
+    for (ch = 'f'; ch <= 'p'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'r'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
