b50b43b5d4be21194f42730f9f3397a3d149adfb
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2023-02-20 07:42:12 -0800

    Printing all the numbers of base 16 in lowercase, followed by a new line using only putchar and three times only

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
+ * Return:(0) is a success
+ */
+int main(void)
+{
+    int n = 48;
+
+	while (n <= 70)
+	{
+	    if (n != 58 && n != 58 && n != 59 && n != 60 && n != 61 && n != 62 && n != 63 && n != 64 );
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
