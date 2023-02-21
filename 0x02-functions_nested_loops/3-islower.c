036c48cf48b96f55e072d90afef12b07aafcd400
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2023-02-21 08:45:14 -0800

    Checking if a character c is lowercase of uppercse

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - checks for a lowercase character
- * and the character to check is c
+ * @c:The character to check is c
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
