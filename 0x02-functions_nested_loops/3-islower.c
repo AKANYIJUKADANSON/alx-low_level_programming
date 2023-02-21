79ad274819d1b00b660a98f6a51395e5b1ad815a
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2023-02-21 08:40:45 -0800

    Checking if a character c is lowercase of uppercse

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower - checks for a lowercase character
+ * and the character to check is c
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
