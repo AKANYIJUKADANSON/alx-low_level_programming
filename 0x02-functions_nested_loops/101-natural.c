67eef23af24259f8ea940126892480c81b95acae
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2023-02-22 03:11:00 -0800

    computes and prints the sum of all the multiples of 3 or 5 below 1024 (excluded), followed by a new line

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,9 +2,9 @@
 
 /**
  * main - computing and printing the sum of all the
- * multiples of 3 or 5 below 1024 (excluded),
+ * multiples of 3 or 5 below 1024(excluded),
  * followed by a new line
- * Return: (0) is a success
+ * Return:(0) is a success
  */
 int main(void)
 {
