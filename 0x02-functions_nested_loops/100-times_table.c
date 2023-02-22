d1a1ddab7f0c54babb946ee7460abc9a8e515d6e
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2023-02-22 03:25:14 -0800

    Printing the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,9 +10,7 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		x = 0;
-
-		while (x <= n)
+		for (x = 0; x <= n; x++)
 		{
 			y = 0;
 
@@ -47,7 +45,6 @@ void print_times_table(int n)
 				y++;
 			}
 			_putchar('\n');
-			x++;
 		}
 	}
 }
