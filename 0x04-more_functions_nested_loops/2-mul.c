98e20c3167d84af4e74d6c2395efa80711d029f6
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2023-02-23 08:10:45 -0800

    Multiplying two digits

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _mul - multiplies two digits
+ * @c: is the first parameter
+ * @b: is another parameter
+ * Return: (0) is always a success
+ */
+int mul(int a, int b)
+{
+	int x;
+	x = x * b;
+	_putchar(x);
+	return (0);
+}
