5c2bae17179d67c816f73dac476e5519aa2f94cb
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2023-03-06 05:47:45 -0800

    Copying memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,18 +2,17 @@
 
 /**
  * *_memcpy - copies memory area
- * @dest: memory area to copy to
+ * @dest: destination memory area
  * @src: memory area to copy from
  * @n: number of bytes to copy
- *
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int x;
-	
+
 	x = 0;
-	while (x < n;)
+	while (x < n)
 	{
 		dest[x] = src[x];
 		x++;
