8e2eb32db5e3425635a328430e231c716c0106ad
AKANYIJUKADANSON <akanyijuka1danson@gmail.com>
2023-03-06 05:34:13 -0800

    Printing a chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints chessboard
+ * @a: parameter
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	x = 0;
+	while (x < 8)
+	{
+		y = 0;
+		while (y < 8)
+		{
+			_putchar(a[x][y]);
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
